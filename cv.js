575ad539a7622de3d50aee8aa0f32fb5f009135f
florianmarin <florian.marin74350@gmail.com>
2016-12-05 16:36:07 +0100

    ajout cv

---
+++ cv.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+   // Stuff to do as soon as the DOM is ready
+
+   $('a#competences').click(function){
+     $('#percu').addClass('animated bounceOutRight ');
+   }//fin de la fonction clik
+
+
+
+
+
+
+
+
+
+
+
+
+
+});//fin du ready
